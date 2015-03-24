ff1c61dc800b35d3c8edba2483ba62b35bd572ad
Michel Boudreau <michelb@pageuppeople.com>
2015-03-24 15:33:37 +1100

    adding watch for autosave or whatever the dev wants to do with it

--- src/angular-restate.js
+++ src/angular-restate.js
@@ -1,213 +1,325 @@
 angular.module('codinghitchhiker.restate', [])
 
-    .constant('HTTPStatus', {
-        UNSPECIFIED: -1,
-        OFFLINE: 0
-    })
+	.constant('HTTPStatus', {
+		UNSPECIFIED: -1,
+		OFFLINE: 0
+	})
 
-    .provider('Restate', function (HTTPStatus) {
-        var http, query, logger;
+	.provider('Restate', function (HTTPStatus) {
+		var http, query, logger, interval, rootScope;
 
-        var baseUrl = '';
-        var defaults = {extend:false};  // TODO: add json diff instead of serializing whole object
-        var models = {};
+		var base = '';
+		var defaults = {extend: false};  // TODO: add json diff instead of serializing whole object
+		var models = {};
 
-        this.setBaseUrl = function (url) {
-            if (angular.isDefined(url)) {
-                url += ''; // Change whatever it is to a string
-            }
-            baseUrl = url || '';
-            return instance;
-        };
+		this.baseUrl = function (url) {
+			// Return value of base is no arguments specified
+			if (arguments.length === 0) {
+				return base;
+			}
 
-        this.config = function (obj) {
-            if (!angular.isObject(obj)) {
-                throw "Config must be an object with key-value pairs"
-            }
-            // TODO: defaults validation?
-            angular.extend(defaults, obj);
-            return instance;
-        };
+			if (angular.isNumber(url)) {
+				url += ''; // Change whatever it is to a string
+			}
+			if (!angular.isString(url)) {
+				throw 'Url is not a Number or a String';
+			}
+			base = url;
+			return instance;
+		};
 
-        this.create = function (label, url, obj, schema) {
-            if(!label || !angular.isString(label)) {
-                throw 'Label must be a specified string';
-            }
-            if(obj != null && !angular.isObject(obj)) {
-                throw 'Object to be wrapped needs to be an Array or an Object';
-            }
-            var model = models[label] || {};
-            url = url || model.$$url || ''; // Can blank to specify base url
-            obj = obj || model.$$obj || null; // Can blank to specify base url
+		this.config = function (obj, clear) {
+			// Return value of defaults is no arguments specified
+			if (arguments.length === 0) {
+				return defaults;
+			}
 
-            var empty = false;
-            if(obj == null) {
-                empty = true;
-                obj = {};
-            }
+			if (!angular.isObject(obj)) {
+				throw "Config must be an object with key-value pairs";
+			}
 
-            addWrap.apply(obj, [label, url, schema, empty]);
+			if (angular.isDefined(obj.extend) && typeof obj.extend !== 'boolean') {
+				throw "Extend property in config must be a Boolean";
+			}
 
-            // Add signature to models
-            models[label] = {$$url:url, $$obj:obj};
+			if (clear) {
+				defaults = angular.copy(obj);
+			} else {
+				angular.extend(defaults, obj);
+			}
 
-            // TODO: add listener to object here
-            return obj;
-        };
+			return instance;
+		};
 
-        var instance = {
-            setBaseUrl: this.setBaseUrl,
-            config: this.config,
-            create: this.create
-        };
+		this.create = function (label, url, obj, schema) {
+			if (!label || !angular.isString(label)) {
+				throw 'Label must be a specified string';
+			}
+			if (obj != null && !angular.isObject(obj)) {
+				throw 'Object to be wrapped needs to be an Array or an Object';
+			}
+			var model = models[label] || {};
+			url = url || model.$$url || ''; // Can blank to specify base url
+			obj = obj || model.$$obj || null; // Can blank to specify base url
 
-        this.$get = function ($http, $q, $log) {
-            http = $http;
-            query = $q;
-            logger = $log;
-            return instance;
-        };
+			var empty = false;
+			if (obj == null) {
+				empty = true;
+				obj = {};
+			}
 
-        function addWrap(label, url, schema, empty) {
-            var that = this;
-            this.$$original = empty?null:angular.copy(this);
-            this.$$label = label;
-            this.$$url = url;
-            this.$$errors = [];
-            this.$$autosave = false;
-            this.$$schema = null;
+			addWrap.apply(obj, [label, url, schema, empty]);
 
-            this.$schema = function (schema) {
-                // TODO: validate schema
-                this.$$schema = schema;
-                runSchema(that, that.$$schema);
-                return this;
-            };
+			// Add signature to models
+			models[label] = {$$url: url, $$obj: obj};
 
-            this.$autosave = function (delay) {
-                // TODO: validate schema
-                this.$$autosave = delay;
-                return this;
-            };
+			return obj;
+		};
 
-            this.$revert = function () {
-                return this;
-            };
+		this.destroy = function (label) {
+			if (!label || !angular.isString(label)) {
+				throw 'Label must be a specified string';
+			}
 
-            // Create http actions
-            angular.forEach(['get', 'delete', 'post', 'put', 'head', 'jsonp', 'patch'], function (method) {
-                // Preface with '$' to ignore serialization
-                that['$' + method] = function (params, data, config) {
-                    return http(angular.extend(defaults, config, {method: method, url: buildUrl(that), params: params, data: toJson(data)})).then(
-                        function (response) {
-                            if(response.data) {
-                                if(response.config.extend && that.$$original != null) {
-                                    if(angular.isArray(response.data) && angular.isArray(that)) {
-                                        that.push.apply(that, response.data);
-                                    }else{
-                                        angular.extend(that, response.data);
-                                    }
-                                    runSchema(that, that.$$schema);
-                                    return that;
-                                }else{
-                                    return instance.create(that.$$label, that.$$url, response.data, that.$$schema);
-                                }
-                            }
-                        },
-                        handleError);
-                };
-            });
+			var model = models[label];
+			if (model) {
+				model.$$destroy();
+				return model;
+			} else {
+				console.warn("Model with label '" + label + "' doesn't exist");
+				return null;
+			}
+		};
 
-            this.$schema(schema);
-        };
+		this.get = function (label) {
+			var model = models[label];
+			if (!label || !angular.isString(label)) {
+				throw 'Label must be a specified string';
+			}
 
-        var runSchema = function(obj, schema) {
-            if(schema != null && !angular.isFunction(schema)) {
-                throw 'Schema is faulty, needs to be a function'
-            }
-            // TODO: create new instances from children in schema
-            if(obj && schema) {
+			if (!model) {
+				throw 'This Model was never specified before';
+			}
 
-            }
-        };
+			var obj = model.$$obj || {};
 
-        // Remove all 'private' variables starting with '$'
-        var toJsonReplacer = function (key, value) {
-            var val = value;
+			addWrap.apply(obj, [label, model.$$url, model.$$schema]);
 
-            if (typeof key === 'string' && key.charAt(0) === '$') {
-                val = undefined;
-            } else if (value && value.window === value) {
-                val = '$WINDOW';
-            } else if (value && document === value) {
-                val = '$DOCUMENT';
-            } else if (value && value.$evalAsync && value.$watch) {
-                val = '$SCOPE';
-            }
+			// TODO: add listener to object here
 
-            return val;
-        };
+			return obj;
+		};
 
-        var toJson = function (obj, pretty) {
-            if (typeof obj === 'undefined') {
-                return undefined;
-            }
-            return JSON.stringify(obj, toJsonReplacer, pretty ? '  ' : null);
-        };
+		var instance = {
+			baseUrl: this.baseUrl,
+			config: this.config,
+			create: this.create,
+			destroy: this.destroy
+		};
 
-        var buildUrl = function (obj) {
-            var url = [], model, label, path = obj.$$label.split('.');
-            for(var i = path.length; i > 0; i--) {
-                label = path.join('.');
-                if(model = models[label]){
-                    url.unshift(model.$$url);
-                }
-                path.pop();
-            }
+		this.$get = function ($http, $q, $log, $rootScope) {
+			http = $http;
+			query = $q;
+			logger = $log;
+			rootScope = $rootScope;
+			return instance;
+		};
 
-            url.unshift(baseUrl);
+		function addWrap(label, url, schema) {
+			// TODO: split this up in different functions for easier maintenance
+			var that = this;
 
-            // TODO: add logic to remove slashes or build a url properly
-            return url.join('/');
-        };
+			var reset = function() {
+				that.$$originalJson = toJson(that);
+				that.$$original = JSON.parse(that.$$originalJson);
+				that.$dirty = false;
+			};
 
-        var handleError = function (response) {
-            // Default error message if not handled properly
-            var error = {
-                id: HTTPStatus.UNSPECIFIED,
-                message: "Something went wrong, we're looking into it",
-                status: response.status
-            };
+			this.$$label = label;
+			this.$$url = url;
+			this.$$errors = [];
+			this.$$saving = false;
+			this.$$schema = null;
+			this.$dirty = false;
 
-            // Check for offline status
-            if (response.status === 0) {
-                error.id = HTTPStatus.OFFLINE;
-                error.message = 'Internet connection not available. Retry later.';
-            }
+			this.$schema = function (schema) {
+				// TODO: validate schema
+				this.$$schema = schema;
+				runSchema(that, that.$$schema);
+				return this;
+			};
 
-            // Check if status is a client error
-            if (response.data && response.status >= 400 && response.status < 500) {
-                error.message = response.data.message;
-                var id;
-                for (var key in HTTPStatus) {
-                    if (HTTPStatus[key] === response.data.id) {
-                        id = HTTPStatus[key];
-                        break;
-                    }
-                }
-                if (!id) {
-                    id = response.data.id;
-                    logger.warn('Error id "' + id + '" is missing from HTTPStatus');
-                }
-                error.id = id;
-            }
+			this.$$destroy = function () {
+				// TODO: add cleanup here in case of watchers
+				scope.$destroy();
+			};
 
-            // Dispatch error on rootscope
-            //$rootScope.$emit('ERROR', error);
+			var scope = {"$destroy": angular.noop};
 
-            // TODO: add logging of said error if possible
+			this.$watch = function (callback, watchExp, equalsFn) {
+				callback = callback || angular.noop;
+				equalsFn = equalsFn || function(value){
+					// TODO: Might not be the best way to do comparison, but does a quick and dirty job
+					return value === that.$$originalJson;
+				};
+				watchExp = watchExp || function () {
+					return toJson(that);
+				};
 
-            return query.reject(error);
-        };
+				// Delete old scope
+				scope.$destroy();
 
-    });
+				// Create new scope with watcher
+				scope = rootScope.$new(true);
+
+				// Watch for changes
+				scope.$watch(watchExp,
+					function (newVal, oldVal) {
+						if(!equalsFn(newVal, oldVal)) {
+							that.$dirty = true;
+						}
+						callback(newVal, oldVal);
+					},
+					true);
+			};
+
+			this.$watch();
+
+			this.$revert = function () {
+				var key;
+				// Remove all keys that doesn't start with $
+				for (key in this) {
+					if (key.charAt(0) != '$') {
+						delete this[key];
+					}
+				}
+
+				// Copy keys over from original object
+				for (key in this.$$original) {
+					if (key.charAt(0) != '$') {
+						this[key] = this.$$original[key];
+					}
+				}
+				that.$dirty = false;
+				return this;
+			};
+
+			// Create http actions
+			angular.forEach(['get', 'delete', 'post', 'put', 'head', 'jsonp', 'patch'], function (method) {
+				// Preface with '$' to ignore serialization
+				that['$' + method] = function (params, data, config) {
+					return http(angular.extend(defaults, config, {method: method, url: buildUrl(that), params: params, data: toJson(data)})).then(
+						function (response) {
+							if (response.data) {
+								if (response.config.extend && that.$$original != null) {
+									if (angular.isArray(response.data) && angular.isArray(that)) {
+										that.push.apply(that, response.data);
+									} else {
+										angular.extend(that, response.data);
+									}
+									runSchema(that, that.$$schema);
+									return that;
+								} else {
+									return instance.create(that.$$label, that.$$url, response.data, that.$$schema);
+								}
+							}
+						},
+						handleError).finally(reset);
+				};
+			});
+
+			this.$schema(schema);
+
+			reset();
+		}
+
+		var runSchema = function (obj, schema) {
+			if (schema != null && !angular.isFunction(schema)) {
+				throw 'Schema is faulty, needs to be a function'
+			}
+			// TODO: create new instances from children in schema
+			if (obj && schema) {
+
+			}
+		};
+
+		// Remove all 'private' variables starting with '$'
+		var toJsonReplacer = function (key, value) {
+			var val = value;
+
+			if (typeof key === 'string' && key.charAt(0) === '$') {
+				val = undefined;
+			} else if (value && value.window === value) {
+				val = '$WINDOW';
+			} else if (value && document === value) {
+				val = '$DOCUMENT';
+			} else if (value && value.$evalAsync && value.$watch) {
+				val = '$SCOPE';
+			}
+
+			return val;
+		};
+
+		var toJson = function (obj, pretty) {
+			if (typeof obj === 'undefined') {
+				return undefined;
+			}
+			return JSON.stringify(obj, toJsonReplacer, pretty ? '  ' : null);
+		};
+
+		var buildUrl = function (obj) {
+			var url = [], model, label, path = obj.$$label.split('.');
+			for (var i = path.length; i > 0; i--) {
+				label = path.join('.');
+				if (model = models[label]) {
+					url.unshift(model.$$url);
+				}
+				path.pop();
+			}
+
+			url.unshift(base);
+
+			// TODO: add logic to remove slashes or build a url properly
+			return url.join('/');
+		};
+
+		var handleError = function (response) {
+			// Default error message if not handled properly
+			var error = {
+				id: HTTPStatus.UNSPECIFIED,
+				message: "Something went wrong, we're looking into it",
+				status: response.status
+			};
+
+			// Check for offline status
+			if (response.status === 0) {
+				error.id = HTTPStatus.OFFLINE;
+				error.message = 'Internet connection not available. Retry later.';
+			}
+
+			// Check if status is a client error
+			if (response.data && response.status >= 400 && response.status < 500) {
+				error.message = response.data.message;
+				var id;
+				for (var key in HTTPStatus) {
+					if (HTTPStatus[key] === response.data.id) {
+						id = HTTPStatus[key];
+						break;
+					}
+				}
+				if (!id) {
+					id = response.data.id;
+					logger.warn('Error id "' + id + '" is missing from HTTPStatus');
+				}
+				error.id = id;
+			}
+
+			// Dispatch error on rootscope
+			//$rootScope.$emit('ERROR', error);
+
+			// TODO: add logging of said error if possible
+
+			return query.reject(error);
+		};
+
+	});
--- src/angular-restate.spec.js
+++ src/angular-restate.spec.js
@@ -1,12 +1,238 @@
 describe('Restate', function () {
-	var base;
-	beforeEach(function () {
-		module('codinghitchhiker.restate');
-		base = 'http://localhost'; // Set initial base url
-	});
+    var base, config, url, label, schema;
 
-	it('Dummy test', inject(function (Restate) {
-		expect(Restate).toBeTruthy();
-	}));
+    beforeEach(function () {
+        module('codinghitchhiker.restate');
+        base = 'http://localhost'; // Set initial base url
+        config = { extend: false };
+        url = 'test';
+        label = 'mahLabel';
+        schema = {};
+    });
 
+    describe('Base Url', function () {
+
+        it('Should be able to set the base url', inject(function (Restate) {
+            expect(Restate.baseUrl(base)).toBeTruthy();
+        }));
+
+        it('Should be able to set the base and retrieve same value', inject(function (Restate) {
+            Restate.baseUrl(base);
+            expect(Restate.baseUrl()).toBe(base);
+        }));
+
+        it('Should be able to set the base as a number', inject(function (Restate) {
+            base = 65487541;
+            Restate.baseUrl(base);
+            expect(Restate.baseUrl()).toBe(base + '');
+        }));
+
+        it('Should throw error on invalid base object', inject(function (Restate) {
+            Restate.baseUrl(base);
+            expect(function () {
+                Restate.baseUrl({});
+            }).toThrow();
+            expect(function () {
+                Restate.baseUrl([]);
+            }).toThrow();
+            expect(Restate.baseUrl()).toBe(base);
+        }));
+
+        it('Should be able to chain Restate calls', inject(function (Restate) {
+            expect(Restate.baseUrl(base)).toBe(Restate);
+            expect(Restate.baseUrl(base).baseUrl).toEqual(jasmine.any(Function));
+        }));
+    });
+
+    describe('Config', function () {
+        it('Should be able to set config', inject(function (Restate) {
+            expect(Restate.config(config)).toBeTruthy();
+        }));
+
+        it('Should be able to set the config and retrieve same object', inject(function (Restate) {
+            Restate.config(config);
+            expect(Restate.config()).toEqual(config);
+        }));
+
+        it('Should be able to set any number of config keys', inject(function (Restate) {
+            config = {
+                something: 123098,
+                another_key: 'blarg'
+            };
+            expect(function () {
+                Restate.config(config);
+            }).not.toThrow();
+        }));
+
+        it('Should be able to extend default without overwriting existing keys', inject(function (Restate) {
+            var defaultConfig = Restate.config();
+            Restate.config({test: 'Blah'});
+            expect(Restate.config()).toEqual(jasmine.objectContaining(defaultConfig));
+        }));
+
+        it('Should throw an error when extend isn\'t a boolean', inject(function (Restate) {
+            config = {extend: 2314};
+            expect(function () {
+                Restate.config(config);
+            }).toThrow();
+            config = {extend: 'asdf'};
+            expect(function () {
+                Restate.config(config);
+            }).toThrow();
+            config = {extend: {}};
+            expect(function () {
+                Restate.config(config);
+            }).toThrow();
+        }));
+
+        it('Should be able to clear default using secondary argument', inject(function (Restate) {
+            config = {akaka: 'ff', ddd: 123};
+            Restate.config(config, true);
+
+            expect(Restate.config()).toEqual(config);
+            expect(Restate.config()).not.toBe(config);
+        }));
+
+        it('Should be able to chain Restate calls', inject(function (Restate) {
+            expect(Restate.config(config)).toBe(Restate);
+            expect(Restate.config(config).config).toEqual(jasmine.any(Function));
+        }));
+    });
+
+    describe('Create', function () {
+        it('Should be able to create stateful object', inject(function (Restate) {
+            expect(Restate.create(label, url, schema)).toBeTruthy();
+        }));
+
+        it('Should be able to create stateful object with schema optional', inject(function (Restate) {
+            expect(Restate.create(label, url)).toBeTruthy();
+        }));
+
+        it('Should throw error on missing label or url arguments', inject(function (Restate) {
+            expect(function () {
+                Restate.create(label);
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(undefined, url);
+            }).toThrow();
+        }));
+
+        it('Should throw error on label collision', inject(function (Restate) {
+            Restate.create(label, url);
+            expect(function () {
+                Restate.create(label, url);
+            }).toThrow();
+
+        }));
+
+        it('Should store arguments within object', inject(function (Restate) {
+            var object = Restate.create(label, url, schema).get(label);
+            expect(object.$$url).toBe(url);
+            expect(object.$$label).toBe(label);
+            expect(object.$$schema).toEqual(schema);
+        }));
+
+        it('Should be able to set absolute or relative url', inject(function (Restate) {
+            var object = Restate.create(label, url).get(label);
+            expect(object.$$url).toBe(url);
+
+            url = 'https://www.google.com';
+            object = Restate.create(label, url).get(label);
+            expect(object.$$url).toBe(url);
+
+            // TODO: Add listeners to http backend, make sure url is correct.
+        }));
+
+        it('Should be able to set parent child relationship through label', inject(function (Restate) {
+            var childlabel = label + '.child';
+            var childurl = 'childurl';
+            Restate
+                .create(label, url, schema)
+                .create(childlabel, childurl, schema);
+
+            expect(Restate.get(childlabel)).toBeTruthy();
+            // TODO: Add listeners to http backend, make sure url is correct.
+        }));
+
+        it('Should be able to build urls', inject(function (Restate) {
+            var childlabel = label + '.child';
+            var childurl = 'childurl';
+            Restate
+                .create(label, url, schema)
+                .create(childlabel, childurl, schema);
+
+            expect(Restate.get(childlabel)).toBeTruthy();
+
+            // TODO: Add listeners to http backend, make sure url is correct.
+        }));
+
+        it('Should throw error when label isn\'t string', inject(function (Restate) {
+            expect(function () {
+                Restate.create(123, url);
+            }).toThrow();
+
+            expect(function () {
+                Restate.create({}, url);
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(true, url);
+            }).toThrow();
+        }));
+
+        it('Should throw error when url isn\'t string or number', inject(function (Restate) {
+            expect(function () {
+                Restate.create(label, []);
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(label, {});
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(label, true);
+            }).toThrow();
+        }));
+
+        it('Should throw error when schema isn\'t array or object', inject(function (Restate) {
+            expect(function () {
+                Restate.create(label, url, 123);
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(label, url, 'weee');
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(label, url, true);
+            }).toThrow();
+
+            expect(function () {
+                Restate.create(label, url, schema);
+            }).not.toThrow();
+        }));
+
+        it('Should be able to chain Restate calls', inject(function (Restate) {
+            var result = Restate.create(label, url, schema);
+            expect(result).toBe(Restate);
+            expect(result.create).toEqual(jasmine.any(Function));
+        }));
+    });
+
+    describe('Get', function () {
+        it('Should be able to set config', inject(function (Restate) {
+            expect(Restate.config(config)).toBeTruthy();
+        }));
+
+        it('Should throw error when parent state missing', inject(function (Restate) {
+            var childlabel = label + '.child';
+            var childurl = 'childurl';
+
+            expect(function () {
+                Restate.create(childlabel, childurl, schema);
+            }).toThrow();
+        }));
+
+    });
 });
